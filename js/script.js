55ea57661777eeb22581fd07c9e1b68622066387
bernard rayoso <rayosobernard@gmail.com>
2024-12-27 23:41:46 +0800

    initialize

--- js/script.js
+++ js/script.js
@@ -1,51 +1,63 @@
- const input = document.getElementById('link-input');
- const linkForm = document.getElementById('link-form');
- const errMsg = document.getElementById('err-msg')
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
 
+const amount = 2000;
 
- linkForm.addEventListener('submit', formSubmit);
+const form = document.getElementById("paymayaForm");
+const errMsg = document.getElementById("err-msg");
 
-// Validate a URL
-function validURL(str) {
-  var pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-      '((\\d{1,3}\\.){3}\\d{1,3}))' +
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-      '(\\?[;&a-z\\d%_.~+=-]*)?' +
-      '(\\#[-a-z\\d_]*)?$',
-    'i'
-  )
-  return !!pattern.test(str)
+form.addEventListener("submit", formSubmit);
+
+// // Validate a URL
+// function validURL(str) {
+//   var pattern = new RegExp(
+//     "^(https?:\\/\\/)?" + // protocol
+//       "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+//       "((\\d{1,3}\\.){3}\\d{1,3}))" +
+//       "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+//       "(\\?[;&a-z\\d%_.~+=-]*)?" +
+//       "(\\#[-a-z\\d_]*)?$",
+//     "i"
+//   );
+//   return !!pattern.test(str);
+// }
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  if (firstName.value === "") {
+    errMsg.innerHTML = "First name is required";
+    input.classList.add("border-red");
+  }
+  if (lastName.value === "") {
+    errMsg.innerHTML = "Last name is required!";
+    input.classList.add("border-red");
+  }
+
+  if (email.value === "") {
+    errMsg.innerHTML = "Email is required!";
+    input.classList.add("border-red");
+  } else {
+    checkout();
+  }
 }
 
+async function checkout() {
+  const id = "28d463cd-ce53-48aa-ba12-b51a66d7a36f";
+  const URL = "https://payments.maya.ph/invoice/payment";
 
- function formSubmit(e) {
-  e.preventDefault()
-
-  if (input.value === '') {
-    errMsg.innerHTML = "Please enter something!"
-    input.classList.add('border-red');
-  } else if (!validURL(input.value)) {
-    errMsg.innerHTML = "Please enter a valid URL!"
-    input.classList.add('border-red');
-  } else {
-    errMsg.innerHTML = ""
-    input.classList.remove('border-red');
-    alert('success')
+  try {
+    const result = await fetch(`${URL}?id=${id}&amount=${amount}`, {
+      method: "GET",
+      redirect: "follow",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then(response => console.log(JSON.stringify(response)));
+  } catch (error) {
+    console.error(error);
   }
-  
-  
-  console.log(input.value)
- }
-
-
- const btn = document.getElementById("menu-btn");
-const menu = document.getElementById("menu");
-btn.addEventListener("click", navToggle);
-
-function navToggle() {
-  btn.classList.toggle("open");
-  menu.classList.toggle("flex");
-  menu.classList.toggle("hidden");
 }
